e5ab891e7bee583bf4e8eb761428ab20c7b7f61c
Mudzia Hutama <mudzia.hutama@gmail.com>
2025-07-01 16:22:08 +0700

    fix: mismatch schema from sendgrid to drizzle

--- libs/external/src/email/adapters/resend/resend-email.service.ts
+++ libs/external/src/email/adapters/resend/resend-email.service.ts
@@ -1,9 +1,9 @@
 import { EnvService } from '@app/env';
 import { AppLogger } from '@app/logger';
+import { ResendEmailEnv } from '@config/email';
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { Resend } from 'resend';
 
-import { ResendSchemaEnv } from '@config/email';
 import { EmailDTO } from '../../dtos';
 import { EmailModule } from '../../email.module';
 import { EmailService } from '../../email.service';
@@ -12,7 +12,7 @@ import { EmailService } from '../../email.service';
 export class ResendEmailService extends Resend implements EmailService, OnModuleInit {
   private readonly logger = new AppLogger(EmailModule.name);
 
-  constructor(private readonly emailConfig: EnvService<ResendSchemaEnv>) {
+  constructor(private readonly emailConfig: EnvService<ResendEmailEnv>) {
     super(emailConfig.get('RESEND_API_KEY'));
   }
 
